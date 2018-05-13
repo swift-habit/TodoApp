7e931f79f474f7bb577ec9ff46953de9ca48c4e9
y0c <wonder789@naver.com>
2018-05-13 13:19:32 +0900

    [Updated] Login Feature Complete closed #4

--- TodoListApp/LoginController.swift
+++ TodoListApp/LoginController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
-
+import Parse
 class LoginController: UIViewController {
 
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var pwTextField: UITextField!
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +25,27 @@ class LoginController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func loginAction(_ sender: Any) {
+        if let id = idTextField.text,
+            let pw = pwTextField.text {
+            self.activityIndicator.startAnimating()
+            PFUser.logInWithUsername(inBackground: id, password: pw) {
+                (user, error) in
+                self.activityIndicator.stopAnimating()
+                if let error = error {
+                    self.alert(message:"오류가 발생하였습니다", title:"알림")
+                } else {
+                    self.alert(message:"환영합니다", title:"알림") {
+                        action in
+                        self.performSegue(withIdentifier: "mainSegue", sender: self)
+                    }
+                    
+                }
+            }
+        }
+        
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -33,3 +57,5 @@ class LoginController: UIViewController {
     */
 
 }
+
+
